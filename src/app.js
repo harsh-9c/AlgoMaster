df07269537cc6faabf3cece1ca8f3d572f532843
Harsh jangitwar <harshjangitwar@Harshs-MacBook-Air.local>
2022-04-28 19:04:48 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,169 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+const hbs = require('hbs');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+var nodemailer = require('nodemailer');
+const auth = require('./middleware/auth');
+
+require('./db/conn');
+
+const Register = require('./models/registers');
+
+const port = process.env.PORT || 3000;
+
+const static_path = path.join(__dirname, '../public');
+const template_path = path.join(__dirname, '../templates/views');
+const partials_path = path.join(__dirname, '../templates/partials');
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(static_path));
+app.set('view engine', 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path);
+
+var transport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'menotify32@gmail.com',
+    pass: 'tafwuc-qyfcys-vykcY4',
+  },
+});
+
+// PAGES
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/question', auth, (req, res) => {
+  res.render('question');
+});
+app.get('/sheet', auth, (req, res) => {
+  res.render('sheet');
+});
+app.get('/editorial', auth, (req, res) => {
+  res.render('editorial');
+});
+
+app.get('/vector', (req, res) => {
+  res.render('vector');
+});
+
+app.get('/binary_search', (req, res) => {
+  res.render('binary_search');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/try', (req, res) => {
+  res.render('try');
+});
+
+// Logout
+
+app.get('/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(curEle => {
+      return curEle.token != req.token;
+    });
+    res.clearCookie('jwt');
+    console.log('Logout Successfully');
+    await req.user.save();
+    res.render('index');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//Registration
+
+app.post('/register', async (req, res) => {
+  try {
+    const password = req.body.password;
+    const cpassword = req.body.confirmpassword;
+    const firstname = req.body.firstname;
+    const email = req.body.email;
+    if (password === cpassword) {
+      const registerEmployee = new Register({
+        firstname: req.body.firstname,
+        email: req.body.email,
+        password: password,
+        confirmpassword: cpassword,
+      });
+
+      const token = await registerEmployee.generateAuthToken();
+
+      res.cookie('jwt', token, {
+        expires: new Date(Date.now() + 60000000),
+        httpOnly: true,
+      });
+
+      var mailOptions = {
+        from: 'menotify32@gmail.com',
+        to: email,
+        subject: 'Welcome to AlgoMaster!',
+        text: 'AlgoMaster is a competitve programming content site. It contains well written and quality code which will help you to upskill your learning. Best Regards AlgoMaster Team.',
+      };
+
+      transport.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent' + info.response);
+        }
+      });
+
+      const registered = await registerEmployee.save();
+      res.status(201).render('login');
+    } else {
+      res.send('passwords are not matching!');
+    }
+  } catch (error) {
+    res.status(400).send('Please input correct details!');
+  }
+});
+
+// Login Check
+
+app.post('/login', async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const useremail = await Register.findOne({ email: email });
+
+    const isMatch = await bcrypt.compare(password, useremail.password);
+
+    const token = await useremail.generateAuthToken();
+    // console.log(token);
+
+    res.cookie('jwt', token, {
+      expires: new Date(Date.now() + 600000),
+      httpOnly: true,
+      // secure: true,
+    });
+
+    if (isMatch) {
+      res.status(201).render('try');
+    } else {
+      res.send('Invalid login details!');
+    }
+  } catch (error) {
+    res.status(400).send('Invalid login details!');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at port no ${port}`);
+});
---
+++ src/models/registers.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
+const { JsonWebTokenError } = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+
+// SCHEMA
+
+const employeeSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  confirmpassword: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+//Generating Tokens
+
+employeeSchema.methods.generateAuthToken = async function () {
+  try {
+    const token = jwt.sign(
+      { _id: this._id.toString() },
+      'process.env.SECRET_KEY'
+    );
+    this.tokens = this.tokens.concat({ token: token }); //key-val are same
+
+    await this.save();
+    return token;
+  } catch (error) {
+    res.send('Errorrrrr ' + error);
+    // console.log("Errorrrrr " + error);
+  }
+};
+
+//Hashing password
+
+employeeSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+    this.confirmpassword = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const Register = mongoose.model('Register', employeeSchema);
+
+module.exports = Register;
